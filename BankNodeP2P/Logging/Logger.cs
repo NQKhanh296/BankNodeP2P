518a5b403fc96969c4460fe46d06b350bc5d0214
tobiasgruber730 <gruber2@spsejecna.cz>
2026-01-28 13:40:46 +0100

    Add logging system (Logger, LogEntry, JSONL store)

---
+++ BankNodeP2P/Logging/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNodeP2P.Logging
+{
+    public class Logger
+    {
+        private readonly LogStoreJsonl _store;
+
+        public event Action<LogEntry>? OnLog;
+
+        public Logger(LogStoreJsonl store)
+        {
+            _store = store;
+        }
+
+        public void Info(string evt, string message = "")
+            => Write("INFO", evt, message);
+
+        public void Warn(string evt, string message = "")
+            => Write("WARN", evt, message);
+
+        public void Error(string evt, string message = "")
+            => Write("ERROR", evt, message);
+
+        private void Write(string level, string evt, string message)
+        {
+            var entry = new LogEntry
+            {
+                Level = level,
+                Event = evt,
+                Message = message
+            };
+
+            _store.Append(entry);
+            OnLog?.Invoke(entry);
+        }
+    }
+}
