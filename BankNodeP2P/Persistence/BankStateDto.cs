4e0aed76c871e043a36494a99d29758b5ec5b374
tobiasgruber730 <gruber2@spsejecna.cz>
2026-01-28 14:23:37 +0100

    Add JSON persistence for bank state

---
+++ BankNodeP2P/Persistence/BankStateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNodeP2P.Persistence
+{
+    public class BankStateDto
+    {
+        public string BankIp { get; set; } = "";
+        public int LastAccountNumber { get; set; }
+        public List<AccountDto> Accounts { get; set; } = new();
+    }
+
+    public class AccountDto
+    {
+        public int Number { get; set; }
+        public long Balance { get; set; }
+    }
+}
---
+++ BankNodeP2P/Persistence/BankStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BankNodeP2P.Persistence
+{
+
+    public class BankStore
+    {
+        private readonly string _path;
+
+        public BankStore(string path)
+        {
+            _path = path;
+            Directory.CreateDirectory(Path.GetDirectoryName(_path)!);
+        }
+
+        public BankStateDto Load()
+        {
+            if (!File.Exists(_path))
+                return new BankStateDto();
+
+            var json = File.ReadAllText(_path);
+            return JsonSerializer.Deserialize<BankStateDto>(json)!;
+        }
+
+        public void SaveAtomic(BankStateDto state)
+        {
+            var tmp = _path + ".tmp";
+            var json = JsonSerializer.Serialize(state, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(tmp, json);
+            File.Move(tmp, _path, true);
+        }
+    }
+}
