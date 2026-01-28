7441c5707c49c7292af14d0ccf0962896f90666d
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2026-01-28 15:26:42 +0100

    created Interface for BankService

---
+++ BankNodeP2P/Protocol/IBankService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNodeP2P.Protocol
+{
+    public interface IBankService
+    {
+        string GetBankIp();                 
+        long GetTotalBalance();            
+        int GetAccountCount();             
+
+        (int accountNumber, string bankIp) CreateAccount();   
+
+        void Deposit(int accountNumber, string bankIp, long amount);    
+        void Withdraw(int accountNumber, string bankIp, long amount);   
+        long GetAccountBalance(int accountNumber, string bankIp);      
+        void RemoveAccount(int accountNumber, string bankIp);
+    }
+}
