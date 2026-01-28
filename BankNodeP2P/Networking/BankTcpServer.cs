b853e7de8f877bf30e160508ebf378dc11221982
NQKhanh296 <156611827+NQKhanh296@users.noreply.github.com>
2026-01-28 15:34:06 +0100

    Added TCP server for handling bank commands

---
+++ BankNodeP2P/Networking/BankTcpServer.cs
@@ -0,0 +1,59 @@
+﻿using BankNodeP2P.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankNodeP2P.Networking
+{
+    public class BankTcpServer
+    {
+        private TcpListener? listener;
+        private CancellationTokenSource? cancellationTokenSource;
+
+        private readonly CommandHandler commandHandler;
+
+        public BankTcpServer(IBankService bankService)
+        {
+            commandHandler = new CommandHandler(bankService);
+        }
+        public void Start(int port)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            Task.Run(() => AcceptLoopAsync(cancellationTokenSource.Token));
+        }
+
+        public void Stop()
+        {
+            cancellationTokenSource?.Cancel();
+            listener?.Stop();
+        }
+
+        private async Task AcceptLoopAsync(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                TcpClient client;
+
+                try
+                {
+                    client = await listener!.AcceptTcpClientAsync(token);
+                }
+                catch
+                {
+                    break;
+                }
+
+                var handler = new ClientHandler(commandHandler);
+                _ = Task.Run(() => handler.RunAsync(client, token), token);
+            }
+        }
+    }
+}
